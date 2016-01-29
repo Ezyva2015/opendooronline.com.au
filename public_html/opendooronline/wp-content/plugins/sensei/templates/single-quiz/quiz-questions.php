cfdd6d12b18ec3cf10a316964f597582c55cda75
Jesus Erwin Suarez <mrjesuserwinsuarez@gmail.com>
2016-01-29 17:35:55 +0800

    Question - add history and answers below question - working

--- public_html/opendooronline/wp-content/plugins/sensei/templates/single-quiz/quiz-questions.php
+++ public_html/opendooronline/wp-content/plugins/sensei/templates/single-quiz/quiz-questions.php
@@ -13,6 +13,8 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 global $post, $woothemes_sensei, $current_user;
 
+//echo "This is sensie <br>";
+
 // Get User Meta
 get_currentuserinfo();
 
@@ -42,6 +44,9 @@ $quiz_passmark_float = (float) $quiz_passmark;
 	$status = WooThemes_Sensei_Utils::sensei_user_quiz_status_message( $quiz_lesson, $current_user->ID );
 	echo '<div class="sensei-message ' . $status['box_class'] . '">' . $status['message'] . '</div>';
 
+
+//	echo "Bellow message <br>";
+
 	// Lesson Quiz Meta
 	if ( 0 < count( $lesson_quiz_questions ) )  {
 		$question_count = 1;
@@ -63,6 +68,12 @@ $quiz_passmark_float = (float) $quiz_passmark;
 
 					$question_count++;
 
+
+//					echo "end loop <br>";
+
+					design($question_item->ID);
+
+					echo "<br> ";
 				} // End For Loop ?>
 
 			</ol>
@@ -74,3 +85,110 @@ $quiz_passmark_float = (float) $quiz_passmark;
 </div>
 
 <?php do_action( 'sensei_quiz_back_link', $quiz_lesson  ); ?>
+
+
+
+
+
+<?php
+
+
+function design($question_id) {
+	$current_user = wp_get_current_user();
+	/**
+	 * @example Safe usage: $current_user = wp_get_current_user();
+	 * if ( !($current_user instanceof WP_User) )
+	 *     return;
+	 */
+//	echo 'Username: ' . $current_user->user_login . '<br />';
+//	echo 'User email: ' . $current_user->user_email . '<br />';
+//	echo 'User first name: ' . $current_user->user_firstname . '<br />';
+//	echo 'User last name: ' . $current_user->user_lastname . '<br />';
+//	echo 'User display name: ' . $current_user->display_name . '<br />';
+//	echo 'User ID: ' . $current_user->ID . '<br />';
+//
+//	echo "This is the footer design of the question <br>";
+//
+
+echo "
+<div>
+";
+
+
+
+
+								//floyd code to get the note history
+
+								$historynotes = get_user_meta($current_user->ID,'_grading_notes_history', true);
+
+								$historyanswers = get_user_meta($current_user->ID,'_grading_answer_history', true);
+
+ 								//echo "<pre>";
+
+								//echo $question_id." ".$lesson_id;
+
+								//print_r($historyanswers);
+
+								//echo "</pre>";
+
+								if(is_array($historynotes)){
+
+
+
+									//print_r($historynotes[$question_id]);
+
+
+
+									if (array_key_exists($question_id, $historynotes) || isset($historynotes[$question_id])) {
+
+										echo "<table class='table striped'>";
+
+										echo "<tr><th colspan='3'>Question History</th></tr>";
+
+										echo "<tr>";
+
+										echo "<th>DATE</th>";
+
+										echo "<th>Answer</th>";
+
+										echo "<th>Notes</th>";
+
+										echo "</tr>";
+
+										foreach($historynotes[$question_id] as $dateTime=>$noteentry){
+
+
+
+											echo "<tr>";
+
+											echo "<td>".$dateTime."</td>";
+
+											echo "<td>".$historyanswers[$question_id][$dateTime]."</td>";
+
+											echo "<td>".$noteentry."</td>";
+
+											echo "</tr>";
+
+
+
+										}
+
+										echo "</table>";
+
+									}
+
+
+
+
+
+								}
+
+
+
+
+
+echo "
+</div>";
+}
+
+?>
